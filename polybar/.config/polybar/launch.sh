258de751636f2d418589ca407b580befb2cd9f95
hackwebdev <hackwebdev@gmail.com>
2021-10-09 01:28:00 +0800

    polybar fixed

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -12,3 +12,18 @@ polybar main &
 if [[ $(xrandr -q | grep 'DP2 connected') ]]; then
 	polybar external &
 fi
+
+
+# #!/usr/bin/env bash
+
+# # Terminate already running bar instances
+# killall -q polybar
+
+# # Wait until the processes have been shut down
+# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# # Launch bar
+# polybar main &
+# #polybar bot
+
+# echo "Bars launched..."
